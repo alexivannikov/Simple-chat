b39d0aac9d9a749c42802258fbce627c9137cbc2
alexivannikov <37912789+alexivannikov@users.noreply.github.com>
2020-10-18 17:49:47 +0300

    Add files via upload

---
+++ ClientWindow.java
@@ -0,0 +1,141 @@
+package com.company;
+
+import javax.accessibility.AccessibleContext;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientWindow extends JFrame {
+    private static final String SERVER_HOST = "localhost";
+    private static final int SERVER_PORT = 8080;
+    private Socket clientSocket;
+    private Scanner inMessage;
+    private PrintWriter outMessage;
+    private JTextField jtfMessage;
+    private JTextField jtfName;
+    private JTextArea jtaTextAreaMessage;
+    private String clientName = "";
+
+    public String getClientName() {
+        return this.clientName;
+    }
+
+    public ClientWindow() {
+        try {
+            clientSocket = new Socket(SERVER_HOST, SERVER_PORT);
+            inMessage = new Scanner(clientSocket.getInputStream());
+            outMessage = new PrintWriter(clientSocket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        setBounds(600, 300, 600, 500);
+        setTitle("Client");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jtaTextAreaMessage = new JTextArea();
+        jtaTextAreaMessage.setEditable(false);
+        jtaTextAreaMessage.setLineWrap(true);
+        JScrollPane jsp = new JScrollPane(jtaTextAreaMessage);
+        add(jsp, BorderLayout.CENTER);
+
+        JLabel jlNumberOfClients = new JLabel("Количество клиентов в чате: ");
+        add(jlNumberOfClients, BorderLayout.NORTH);
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        add(bottomPanel, BorderLayout.SOUTH);
+        JButton jbSendMessage = new JButton("Отправить");
+        bottomPanel.add(jbSendMessage, BorderLayout.EAST);
+        jtfMessage = new JTextField("Введите ваше сообщение: ");
+        bottomPanel.add(jtfMessage, BorderLayout.CENTER);
+        jtfName = new JTextField("Введите ваше имя: ");
+        bottomPanel.add(jtfName, BorderLayout.WEST);
+        // обработчик события нажатия кнопки отправки сообщения
+        jbSendMessage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // если имя клиента, и сообщение непустые, то отправляем сообщение
+                if (!jtfMessage.getText().trim().isEmpty() && !jtfName.getText().trim().isEmpty()) {
+                    clientName = jtfName.getText();
+                    sendMsg();
+                    // фокус на текстовое поле с сообщением
+                    jtfMessage.grabFocus();
+                }
+            }
+        });
+        jtfMessage.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                jtfMessage.setText("");
+            }
+        });
+        jtfName.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                jtfName.setText("");
+            }
+        });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // бесконечный цикл
+                    while (true) {
+                        // если есть входящее сообщение
+                        if (inMessage.hasNext()) {
+                            // считываем его
+                            String inMes = inMessage.nextLine();
+                            String clientsInChat = "Клиентов в чате = ";
+                            if (inMes.indexOf(clientsInChat) == 0) {
+                                jlNumberOfClients.setText(inMes);
+                            } else {
+                                // выводим сообщение
+                                jtaTextAreaMessage.append(inMes);
+                                // добавляем строку перехода
+                                jtaTextAreaMessage.append("\n");
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                }
+            }
+        }).start();
+        // добавляем обработчик события закрытия окна клиентского приложения
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                try {
+                    // здесь проверяем, что имя клиента непустое и не равно значению по умолчанию
+                    if (!clientName.isEmpty() && clientName != "Введите ваше имя: ") {
+                        outMessage.println(clientName + " вышел из чата!");
+                    } else {
+                        outMessage.println("Участник вышел из чата, так и не представившись!");
+                    }
+                    // отправляем служебное сообщение, которое является признаком того, что клиент вышел из чата
+                    outMessage.println("##session##end##");
+                    outMessage.flush();
+                    outMessage.close();
+                    inMessage.close();
+                    clientSocket.close();
+                } catch (IOException exc) {
+
+                }
+            }
+        });
+        // отображаем форму
+        setVisible(true);
+    }
+
+    // отправка сообщения
+    public void sendMsg() {
+        // формируем сообщение для отправки на сервер
+        String messageStr = jtfName.getText() + ": " + jtfMessage.getText();
+        // отправляем сообщение
+        outMessage.println(messageStr);
+        outMessage.flush();
+        jtfMessage.setText("");
+    }
+}
